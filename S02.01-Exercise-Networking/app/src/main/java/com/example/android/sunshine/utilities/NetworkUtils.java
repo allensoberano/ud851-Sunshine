3320ecfbcceda2abe0bf45c492a8a0df3ef7f932
Allen Soberano <allensoberano@gmail.com>
2018-01-24 05:05:21 -0800

    Lesson02.02 completed

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -57,6 +60,7 @@ public final class NetworkUtils {
     final static String UNITS_PARAM = "units";
     final static String DAYS_PARAM = "cnt";
 
+
     /**
      * Builds the URL used to talk to the weather server using a location. This location is based
      * on the query capabilities of the weather provider that we are using.
@@ -64,10 +68,37 @@ public final class NetworkUtils {
      * @param locationQuery The location that will be queried for.
      * @return The URL to use to query the weather server.
      */
-    public static URL buildUrl(String locationQuery) {
-        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
-        return null;
-    }
+
+    // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
+        /**
+         * Builds the URL used to query weather
+         *
+         * @param locationQuery The keyword that will be queried for.
+         * @return The URL to use to query the weather server.
+         */
+
+        public static URL buildUrl(String locationQuery) {
+            // COMPLETED (1) Fill in this method to build the proper Github query URL
+
+            //*URI ANDROID FRAMEWORK
+            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, locationQuery)
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .build();
+
+
+            //*URL JAVA
+            URL url = null;
+            try {
+                url = new URL(builtUri.toString());
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            }
+
+            return url;
+        }
 
     /**
      * Builds the URL used to talk to the weather server using latitude and longitude of a
